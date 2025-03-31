5cb47b206590238840be5f0d9060c953646ffbaf
Wenfei_ <2185006560@qq.com>
2025-03-31 16:23:42 +0800

    更新

---
+++ ICP.js
@@ -0,0 +1,33 @@
+ 
+// ICP.js - 备案信息数据集
+window.icpData = {
+    "彬ICP备12345678号": {
+        icpNumber: "彬ICP备12345678号",
+        unit: "此为测试网站标题",
+        auditTime: "2025-03-31",
+        nature: "企业/个人",
+        status: "域名"
+    },
+    "彬ICP备20258956号": {
+        icpNumber: "彬ICP备20258956号",
+        unit: "文天玩网络科技有限公司",
+        auditTime: "2025-03-31",
+        nature: "企业",
+        status: "wentianplay.com"
+    },
+    "彬ICP备20254272号": {
+        icpNumber: "彬ICP备20254272号",
+        unit: "文天玩网络科技有限公司",
+        auditTime: "2025-03-31",
+        nature: "企业",
+        status: "wentianplay.net"
+    },
+    "彬ICP备20252823号": {
+        icpNumber: "彬ICP备20252823号",
+        unit: "文天玩网络科技有限公司",
+        auditTime: "2025-03-31",
+        nature: "企业",
+        status: "wentianplay.top"
+    }
+};
+ 
