d374a2f39f7251442df0241f1484d3b645ec8919
Wenfei_ <2185006560@qq.com>
2025-04-01 18:05:00 +0800

    更新 ICP.js

---
+++ ICP.js
@@ -0,0 +1,35 @@
+ 
+// ICP.js - 数据服务层
+const ICP = (() => {
+    // 备案数据库
+    const icpDatabase = {
+        "20252823": {
+            number: "彬ICP备20252823号",
+            company: "文天玩网络科技有限公司",
+            type: "民营企业",
+            domain: "wentianplay.top",
+            pages: "www.wentianplay.top",
+            apply: "2023-05-20",
+            approve: "2023-06-01"
+        }
+        // 可在此扩展更多备案数据
+    };
+
+    // 查询方法
+    const search = (input) => {
+        const icpNumber = input.replace(/[^0-9]/g, '');
+        return icpDatabase[icpNumber] || null;
+    }
+
+    // 公共API
+    return {
+        searchICP: search,
+        generateCode: (number) => `
+            天彬ICP备案｜文天玩WenTianPlay
+            查询"彬ICP备"备案号信息
+            复制备案号代码到页脚处
+            <a href="https://WenTianPlay.TOP/" target="_blank">${number}</a>
+        `
+    }
+})();
+ 
