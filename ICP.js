001610e72592cbfa603d69626019426eb9ec3b83
Wenfei_ <2185006560@qq.com>
2025-05-12 13:23:26 +0800

    更新 ICP.js

--- ICP.js
+++ ICP.js
@@ -1,36 +1,4 @@
  
-// ICP.js - 数据服务层
-const ICP = (() => {
-    // 备案数据库
-    const icpDatabase = {
-        "20252823": {
-            number: "彬ICP备20252823号",
-            company: "文天玩网络科技有限公司",
-            type: "民营企业",
-            domain: "WenTianPlay.TOP",
-            pages: "www.wentianplay.top*",
-            apply: "2025-03-31",
-            approve: "2025-03-31"
-        },
-        "20254272": {
-            number: "彬ICP备20254272号",
-            company: "文天玩网络科技有限公司",
-            type: "民营企业",
-            domain: "WenTianPlay.NET",
-            pages: "www.wentianplay.net*",
-            apply: "2025-03-31",
-            approve: "2025-03-31"
-        },
-        "20258956": {
-            number: "彬ICP备20258956号",
-            company: "文天玩网络科技有限公司",
-            type: "民营企业",
-            domain: "WenTianPlay.COM",
-            pages: "www.wentianplay.com*",
-            apply: "2025-03-31",
-            approve: "2025-03-31"
-         
-// ICP备案数据库
 const ICP = (() => {
     const database = {
         "20252823": {
@@ -77,10 +45,9 @@ const ICP = (() => {
             pages: "vkcraftminecraft.com*",
             apply: "2025-04-01",
             approve: "2025-04-01"
-    }};
+        }
+    };
 
-
-    // 查询方法
     const search = (input) => {
         const icpNumber = input.replace(/[^\d]/g, '');
         return database[icpNumber] || null;
